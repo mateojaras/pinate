585946442791503b0824f52efef174af51af64b5
mateojaras <mateo.jaramillo@sofka.com.co>
2021-11-09 00:14:07 -0500

    se integra ant desing

--- front-react/front/src/graficos/paginas/Principal.jsx
+++ front-react/front/src/graficos/paginas/Principal.jsx
@@ -1,37 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Tarjeta from '../../componentes/Tarjeta';
-import ConexionesProducto from '../../conexiones/api/ConexionesProducto';
-import NavbarSide from '../../componentes/NavbarSide';
-import { useDispatch, useSelector } from 'react-redux';
-import { ActualizarCategoria } from '../../Dominio/actions/actionProducto';
-
+import Tarjeta from "../../componentes/Tarjeta";
+import ConexionesProducto from "../../conexiones/api/ConexionesProducto";
+import NavbarSide from "../../componentes/NavbarSide";
+import { useDispatch, useSelector } from "react-redux";
+import { ActualizarCategoria } from "../../Dominio/actions/actionProducto";
+import { Col, DatePicker, Row } from "antd";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 export default function Principal() {
+  const state = useSelector((state) => state);
 
-    const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  useEffect(async () => {
+    dispatch(ActualizarCategoria(await ConexionesProducto.getListProducts()));
+  }, [dispatch]);
 
-    useEffect(async () => {
-        dispatch(ActualizarCategoria(await ConexionesProducto.getListProducts()))
-    }, [dispatch])
-
-
-    return (
-        <div>
-            
-            <div class="cards-list">
-            <NavbarSide />
-                {
-                    state.map(prod => {
-                        return <Tarjeta props={{ nombre: prod.nombre, foto: prod.foto }} />
-                    }
-
-                    )
-                }
-            </div >
-        </div>
-
-    )
+  return (
+    <Row>
+      <Col span={6}>
+        <NavbarSide />
+      </Col>
+      <Col span={8}>
+      <div class="cards-list">
+        {state.map((prod) => {
+          return <Tarjeta props={{ nombre: prod.nombre, foto: prod.foto }} />;
+        })}
+      </div>
+      </Col>
+    </Row>
+  );
 }
