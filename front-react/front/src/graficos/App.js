2318a36e8b54e18129c3d76b0a9473c164f83cf3
mateojaras <mateo.jaramillo@sofka.com.co>
2022-01-04 22:42:06 -0500

    add prueba

--- front-react/front/src/graficos/App.js
+++ front-react/front/src/graficos/App.js
@@ -1,41 +1,58 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from "../componentes/PrivateRoute";
 import PublicRoute from "../componentes/PublicRouter";
 import { auth } from "../conexiones/firebase/firebase";
 import CrearProducto from "./paginas/CrearProducto";
 import InicioSesion from "./paginas/InicioSesion";
+import Prueba from "./paginas/Prueba";
 import Tablero from "./paginas/Tablero";
 
-
 function App() {
+	const [autenticado, setautenticado] = useState(false);
 
-  const [autenticado, setautenticado] = useState(false)
+	useEffect(() => {
+		auth().onAuthStateChanged((user) => {
+			if (user) {
+				setautenticado(true);
+			} else {
+				setautenticado(false);
+			}
 
-  useEffect(() => {
-    auth().onAuthStateChanged((user) => {
-      if(user){
-        setautenticado(true);
-      }
-      else{
-        setautenticado(false);
-      }
-      
-      console.log(auth().currentUser)
-    })
-}, [])
+			console.log(auth().currentUser);
+		});
+	}, []);
 
-
-  return (
-    <Router>
-      <Switch>
-        
-        <PublicRoute exact path={"/"} authenticated={autenticado} component={InicioSesion} />
-        <PrivateRoute exact path={"/principal"} authenticated={autenticado} component={Tablero}/>
-        <PrivateRoute exact path={"/crear"} authenticated={autenticado} component={CrearProducto} />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<PublicRoute
+					exact
+					path={"/"}
+					authenticated={autenticado}
+					component={InicioSesion}
+				/>
+				<PrivateRoute
+					exact
+					path={"/principal"}
+					authenticated={autenticado}
+					component={Tablero}
+				/>
+				<PrivateRoute
+					exact
+					path={"/crear"}
+					authenticated={autenticado}
+					component={CrearProducto}
+				/>
+				<PublicRoute
+					exact
+					path={"/prueba"}
+					authenticated={autenticado}
+					component={Prueba}
+				/>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
